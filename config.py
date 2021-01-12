4e81212bfdef84e19cfdf33aabf50b2b675a7f5d
Fraol Abebe <fraolabebe@Fraols-Air.fios-router.home>
2021-01-12 01:44:01 -0500

    configuring

--- config.py
+++
@@ -1,9 +0,0 @@
-sf_username = 'abebefraol@gmail.com' 
-sf_password = 'Ethiopia2011#' 
-sf_security_token = 'epqNIN7jh5Cprino5avkLytHH' 
-
-remote_db_endpoint = 'databootcamp.crllqd59ljli.us-east-2.rds.amazonaws.com'
-remote_db_port = '3306'
-remote_db_user = 'admin'
-remote_db_pwd = 'Ethiopia2011'
-remote_db_name = 'gwsis'
